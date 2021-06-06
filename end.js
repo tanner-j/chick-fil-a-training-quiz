06d9ce4aecad7b8b4cb8f308e7cac6c1b0c2e143
tanner-j <tjohn412@students.kennesaw.edu>
2021-06-06 17:54:00 -0400

    another path fix

--- end.js
+++ end.js
@@ -26,5 +26,5 @@ saveHighScore = (e) => {
     highScores.splice(MAX_HIGH_SCORES);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign("/");
+    window.location.assign("index.html");
 }
